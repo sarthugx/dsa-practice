d040fdc97eae8271eb4820b86dbb7524721ce368
sarthug <smpingale2469@gmail.com>
2026-06-30 14:40:48 +0530

    Time: 75 ms (8.42%), Space: 56 MB (6.44%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,7 +1,24 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        Arrays.sort(nums);
-        int n = nums.length;
-        return nums[n/2];
+
+        // Insertion sort
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i + 1; j > 0; j--) {
+                if (nums[j] < nums[j - 1]) {
+                    swap(nums, j, j - 1);
+                } else {
+                    break;
+                }
+            }
+        }
+
+        // Majority element
+        return nums[nums.length / 2];
+    }
+
+    public void swap(int[] nums, int a, int b) {
+        int temp = nums[a];
+        nums[a] = nums[b];
+        nums[b] = temp;
     }
 }
