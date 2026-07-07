4d6594b4ab2528e2dea50f0c05950bdcb1ec113f
sarthug <smpingale2469@gmail.com>
2026-07-08 00:51:33 +0530

    Time: 23 ms (93.71%), Space: 48.6 MB (78.75%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -1,25 +1,28 @@
 class Solution {
     public int subarraySum(int[] arr, int k) {
+
         Map<Integer, Integer> map = new HashMap<>();
-        
-        // Base case: A prefix sum of 0 has been seen exactly 1 time (before starting)
-        map.put(0, 1);
-        
+
         int count = 0;
         int prefSum = 0;
-        
-        for (int i = 0; i < arr.length; ++i) {
+
+        map.put(0, 1);
+
+        for (int i = 0; i < arr.length; i++) {
+
             prefSum += arr[i];
-            
-            // If (prefSum - k) exists, add its frequency to our count
+
             if (map.containsKey(prefSum - k)) {
-                count += map.get(prefSum - k); // FIXED BUG 1
+                count += map.get(prefSum - k);
             }
-            
-            // Keep track of how many times we've seen this prefSum
-            // FIXED BUG 2: Always update frequencies, never skip!
-            map.put(prefSum, map.getOrDefault(prefSum, 0) + 1);
+
+            if (map.containsKey(prefSum)) {
+                map.put(prefSum, map.get(prefSum) + 1);
+            } else {
+                map.put(prefSum, 1);
+            }
         }
+
         return count;
     }
 }
