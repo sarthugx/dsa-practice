c094387d8b8c96b77c933144c4a8edc51b0b46c3
sarthug <smpingale2469@gmail.com>
2026-07-25 02:48:58 +0530

    Time: 903 ms (7.47%), Space: 60 MB (16.89%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,30 +1,40 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        if (nums == null || nums.length<3){
+        
+        if(nums == null || nums.length < 3){
             return new ArrayList<>();
         }
-
+        
         Arrays.sort(nums);
-        Set <List<Integer>> result = new HashSet<>();
+        //using set so same same pairs will not be at the end
+        Set <List<Integer>
+        > set = new HashSet<>();
 
-        for (int i=0; i< nums.length -2; i++){
+        for(int i = 0; i < nums.length -2; i++){
             int left = i+1;
-            int right = nums.length-1;
+            int right = nums.length -1;
 
             while(left < right){
                 int sum = nums[i] + nums[left] + nums[right];
-                
+
                 if(sum == 0){
-                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    set.add(Arrays.asList(nums[i], nums[left], nums[right]));
                     left ++;
-                    right--;
-                }else if(sum <0){
-                    left++;
+                    right --;
+                } else if(sum < 0){
+                    left ++;
                 }else{
-                    right--;
+                    right --;
                 }
             }
         }
-        return new ArrayList<>(result);
+
+        return new ArrayList<>(set);
     }
 }
+
+
+//[1 2 3 4 5 6]
+//so what we are gonna do is we will just have a for loop going which will fix one element at a time
+//after fixing one ele we will find remaing two using left and right, using same method as two sum
+//
