59d93d66da32f49eec583395e1f777a184286095
sarthug <smpingale2469@gmail.com>
2026-08-16 16:39:40 +0530

    Time: 1 ms (99.76%), Space: 47.4 MB (38.88%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int jump(int[] nums) {
+        int totalJumps = 0;
+
+        int destination = nums.length -1;
+
+        int coverage = 0;
+        int lastJumpIdx = 0;
+
+        if(nums.length == 1) return 0;
+
+        for(int i = 0; i<nums.length; i++){
+            coverage = Math.max(coverage, i+nums[i]);
+
+            if(i == lastJumpIdx){
+                lastJumpIdx = coverage;
+                totalJumps++;
+
+                if(coverage >= destination){
+                    return totalJumps;
+                }
+            }
+        }
+        
+        return totalJumps;
+    }
+}
