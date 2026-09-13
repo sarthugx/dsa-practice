0f397eebbf8d68865408ec42a336725e63d7b4be
sarthug <smpingale2469@gmail.com>
2026-09-13 23:41:34 +0530

    Time: 0 ms (100%), Space: 47.8 MB (70.3%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,19 +1,38 @@
 class Solution {
     public double myPow(double x, int n) {
-        if(n<0){
-            n=n*-1;
-            x=1/x;
-        }
+        // if(n<0){
+        //     n=n*-1;
+        //     x=1/x;
+        // }
         
-        double result =1;
-        while(n!=0){
-            if(n%2!=0){
-                result = result * x;
-            }
+        // double result =1;
+        // while(n!=0){
+        //     if(n%2!=0){
+        //         result = result * x;
+        //     }
 
-            x= x*x;
-            n= n/2;
-        }
-        return result;
+        //     x= x*x;
+        //     n= n/2;
+        // }
+        // return result;
+
+        //using recursion
+        
+        //checkling if n is neg or pos
+        
+        long exponent = n;
+
+        if(exponent < 0) return 1.0/power(x, -exponent);
+
+        return power(x,exponent);
+    }
+
+    private double power(double x, long n){
+        if(n == 0) return 1;
+        if(n == 1) return x;
+
+        if(n % 2 == 0) return power(x*x, n/2);
+        
+        return x * power(x, n-1);
     }
 }
